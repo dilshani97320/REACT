21e91fb46953d709de8017cf043277b44d8fd5a0
[dilshani] <[dilshanithathsara@gmail.com]>
2021-04-24 22:41:52 +0530

    first attempt

---
+++ recipe-app/src/Recipe.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const Recipe =()=>{
+    return(
+        <div>
+            <h1>Title</h1>
+            <p>Calories</p>
+            <img src="" alt=""/>
+        </div>
+    );
+}
+export default Recipe;
