74a351c69149dcbf160483d14208981eda1d28ef
[dilshani] <[dilshanithathsara@gmail.com]>
2021-05-08 23:52:11 +0530

    complete 80%

--- website1/src/App.js
+++ website1/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Navbar from './component/Navbar';
-import {BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './component/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <switch>
+        <Route path='/' exact component={Home}/>
+      </switch>
     </Router>
 
   );
--- website1/src/component/Button.js
+++ website1/src/component/Button.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import './Button.css';
 
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const STYLE=['btn--primary', 'btn--outline'];
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-const SIZES =['btn--medium' , 'btn--large' , 'btn--mobile','btn--wide'];
-
-const COLOR =['primary' ,'blue', 'red' ,'green'];
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
-    children ,
-    type,
-    onClick ,
-    buttonStyle ,
-    buttonSize,
-    buttonColor}) =>{
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+  buttonColor
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-const checkButtonStyle=STYLE.includes(buttonStyle) ?
-buttonStyle:STYLE[0];
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
-const checkButtonSize=STYLE.includes(buttonSize) ?
-buttonSize:SIZES[0];
-
-const checkButtonColor=STYLE.includes(buttonColor) ?
-buttonColor:null;
-
-        return(
-        <button className={'btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}'} onClick={onClick} type={type}>{children}</button>
-        )
-    };
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
